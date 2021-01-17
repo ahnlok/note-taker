71371f8858247bb4e5734940d14dbe94734c3f59
Sungpil An <ansungpil1@gmail.com>
2021-01-17 12:43:50 -0500

    added retrieving and delete function for id in route.js

--- routes/route.js
+++ routes/route.js
@@ -10,13 +10,24 @@ module.exports = function(app) {
         const notes = JSON.parse(data);
         // GET api/notes route
         app.get("/api/notes", function(req, res) {
-            res.json()
+            res.json(notes)
         });
         // POST api/notes route
         app.post("/api/notes", function(req, res) {
             const newNote = req.body;
             notes.push(newNote);
             updateDb();
+            return console.log("Added new note: "+newNote.title);
+        });
+        // Retrieving a note with id
+        app.get("/api/notes/:id", function(req, res) {
+            res.json(notes[req.params.id]);
+        });
+        // Delete the note with selected id
+        app.delete("/api/notes/:id", function(req, res) {
+            notes.splice(req.params.id, 1);
+            updateDb();
+            console.log("Deleted note with id "+req.params.id);
         }
         }
 }
